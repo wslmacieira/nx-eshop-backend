eafd297963760cb8e76ea654acbf58a35a876f1d
wslmacieira <wslmacieira@outlook.com>
2021-06-26 15:24:41 -0300

    register new user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,7 @@
-const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
+
+const {User} = require('../models/user');
 
 router.get('/', async (req, res) =>{
     const userList = await User.find();
@@ -9,6 +10,27 @@ router.get('/', async (req, res) =>{
         res.status(500).json({success: false})
     } 
     res.send(userList);
-})
+});
+
+router.post('/', async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: req.body.passwordHash,
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country
+  });
+  user = await user.save();
+
+  if (!user) {
+    return res.status(404).send('the user cannot be created!');
+  }
+  res.send(user);
+});
 
 module.exports = router;
