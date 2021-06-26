df648d55a49f9b700d28bd5176405caeb53aa3fb
wslmacieira <wslmacieira@outlook.com>
2021-06-26 01:02:26 -0300

    enabling cors

--- backend/app.js
+++ backend/app.js
@@ -2,24 +2,32 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
-
+const cors = require('cors')
 require('dotenv/config');
 const api = process.env.API_URL;
-const productsRouter = require('./routes/products');
 
+app.use(cors());
+app.options('*', cors())
 
 app.use(express.json());
 app.use(morgan('tiny'));
 
-// Routers
-app.use(`${api}/products`, productsRouter)
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
 
-const Product = require('./models/product');
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
 
 mongoose.connect(process.env.CONNECTION_STRING, {
   useNewUrlParser: true,
-  useUnifiedTopology:true
+  useUnifiedTopology:true,
+  dbName: 'eshop-database'
 })
   .then(() => {
     console.log('Database Connection is ready...')
