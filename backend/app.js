f74f2d7bed561d6c85d4b96db867098467bac265
wslmacieira <wslmacieira@outlook.com>
2021-06-25 23:17:20 -0300

    logging API requests

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 require('dotenv/config');
 
 const api = process.env.API_URL;
 
 app.use(express.json());
+app.use(morgan('tiny'));
 
 // http://localhost:3000/api/v1/products
 app.get(`${api}/products`, (req, res) => {
