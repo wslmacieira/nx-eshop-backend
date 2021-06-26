e3626ef0c0961e4dc12d1edf68a8ffaaba1b365f
wslmacieira <wslmacieira@outlook.com>
2021-06-26 00:36:39 -0300

    read write data to database using API

--- backend/app.js
+++ backend/app.js
@@ -10,20 +10,41 @@ const api = process.env.API_URL;
 app.use(express.json());
 app.use(morgan('tiny'));
 
-// http://localhost:3000/api/v1/products
-app.get(`${api}/products`, (req, res) => {
-  const product = {
-    id: 1,
-    name: 'hair dresser',
-    image: 'some_url'
+const productSchema = mongoose.Schema({
+  name: String,
+  image: String,
+  countInStock: {
+    type: Number,
+    required: true
   }
-  res.send(product);
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+// http://localhost:3000/api/v1/products
+app.get(`${api}/products`, async(req, res) => {
+  const productList = await Product.find();
+  if (!productList) {
+    res.status(500).json({success: false})
+  }
+  res.send(productList);
 });
 
 app.post(`${api}/products`, (req, res) => {
-  const newProduct = req.body;
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock
+  });
 
-  res.send(newProduct);
+  product.save().then((createdProduct => {
+    res.status(201).json(createdProduct)
+  })).catch((err) => {
+    res.status(500).json({
+      error: err,
+      success: false
+    })
+  })
 });
 
 mongoose.connect(process.env.CONNECTION_STRING, {
