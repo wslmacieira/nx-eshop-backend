db824b4b3d84c74d894fc0284d6208ee12fee9a9
wslmacieira <wslmacieira@outlook.com>
2021-06-26 09:10:08 -0300

    get categories and category details

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -6,10 +6,19 @@ router.get('/', async (req, res) =>{
     const categoryList = await Category.find();
 
     if(!categoryList) {
-        res.status(500).json({success: false});
+        res.status(404).json({success: false});
     } 
-    res.send(categoryList);
+    res.status(200).send(categoryList);
 });
+
+router.get('/:id', async (req, res) => {
+  const category = await Category.findById(req.params.id);
+
+  if(!category) {
+    res.status(404).json({message: 'the category with the given ID was not found!'})
+  }
+  res.status(200).send(category);
+})
 
 router.post('/', async (req, res) => {
   let category = new Category({
