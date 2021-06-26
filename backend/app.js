8c6228ae8fbc86896868e601191e4c7f0314d9e6
wslmacieira <wslmacieira@outlook.com>
2021-06-25 23:09:46 -0300

    create first api call

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,24 @@ require('dotenv/config');
 
 const api = process.env.API_URL;
 
+app.use(express.json());
+
 // http://localhost:3000/api/v1/products
-app.get(api+'/products', (req, res) => {
-  res.send('hello API');
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: 'hair dresser',
+    image: 'some_url'
+  }
+  res.send(product);
+});
+
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+
+  res.send(newProduct);
 });
 
 app.listen(3000, () => {
-  console.log(api)
   console.log('🚀 Server runing in http://localhost:3000')
 });
