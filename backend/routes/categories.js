88593b0444a64557f822ceb30796fa3531cc8789
wslmacieira <wslmacieira@outlook.com>
2021-06-26 09:20:18 -0300

    update category

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,21 +1,21 @@
-const {Category} = require('../models/category');
+const { Category } = require('../models/category');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req, res) =>{
-    const categoryList = await Category.find();
+router.get('/', async (req, res) => {
+  const categoryList = await Category.find();
 
-    if(!categoryList) {
-        res.status(404).json({success: false});
-    } 
-    res.status(200).send(categoryList);
+  if (!categoryList) {
+    res.status(404).json({ success: false });
+  }
+  res.status(200).send(categoryList);
 });
 
 router.get('/:id', async (req, res) => {
   const category = await Category.findById(req.params.id);
 
-  if(!category) {
-    res.status(404).json({message: 'the category with the given ID was not found!'})
+  if (!category) {
+    res.status(404).json({ message: 'the category with the given ID was not found!' })
   }
   res.status(200).send(category);
 })
@@ -28,8 +28,24 @@ router.post('/', async (req, res) => {
   });
   category = await category.save();
 
-  if(!category) {
+  if (!category) {
     return res.status(404).send('the category cannot be created!');
+  }
+  res.send(category);
+});
+
+router.put('/:id', async (req, res) => {
+  const category = await Category.findByIdAndUpdate(
+    req.params.id, 
+    {
+    name: req.body.name,
+    icon: req.body.icon,
+    color: req.body.color
+    },
+    {new: true}
+  );
+  if (!category) {
+    return res.status(404).send('the category cannot be updated!');
   }
   res.send(category);
 });
@@ -37,11 +53,11 @@ router.post('/', async (req, res) => {
 router.delete('/:id', (req, res) => {
   Category.findByIdAndRemove(req.params.id).then((category) => {
     if (!category) {
-      return res.status(404).json({success: false, message: 'category not found!'})
+      return res.status(404).json({ success: false, message: 'category not found!' })
     }
-    return res.status(200).json({success: true, message: 'the category is deleted!'})
+    return res.status(200).json({ success: true, message: 'the category is deleted!' })
   }).catch((err) => {
-    return res.status(400).json({success: false, error: err})
+    return res.status(400).json({ success: false, error: err })
   })
 });
 
