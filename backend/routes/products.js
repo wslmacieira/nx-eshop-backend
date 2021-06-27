e408edd916c1206d4191e5e07e78a39981704f8d
wslmacieira <wslmacieira@outlook.com>
2021-06-27 00:42:15 -0300

    configure server side upload

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const router = express.Router();
+const multer = require('multer');
 
 const {Product} = require('../models/product');
 const {Category} = require('../models/category');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/public/upload')
+  },
+  filename: function (req, file, cb) {
+    const fileName = file.originalname.split(' ').join('-')
+    cb(null, fileName + '-' + Date.now())
+  }
+});
+
+const uploadOptions = multer({ storage: storage})
 
 router.get('/', async (req, res) =>{
   let filter = {};
@@ -28,17 +41,19 @@ router.get('/:id', async (req, res) =>{
   res.send(product);
 });
 
-router.post('/', async (req, res) =>{
+router.post('/', uploadOptions.single('image'), async (req, res) =>{
   const category = await Category.findById(req.body.category);
   if (!category) {
     return res.status(400).send('Invalid category');
   }
+  const fileName = req.file.filename
+  const basePath = `${req.protocol}//${req.get('host')}/public/upload/`;
 
   let product = new Product({
     name: req.body.name,
     description: req.body.description,
     richDescription: req.body.richDescription,
-    image: req.body.image,
+    image: `${basePath}${fileName}`, //"http://localhost:3000/public/upload/image-2222"
     brand: req.body.brand,
     price: req.body.price,
     category: req.body.category,
