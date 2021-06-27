40cc9fb2fd4ebea2764fbec933cebd7f24641e0a
wslmacieira <wslmacieira@outlook.com>
2021-06-26 23:37:01 -0300

    calculating total price

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -42,6 +42,16 @@ router.post('/', async (req, res) => {
 
   const orderItemsIdsResolved = await orderItemsIds;
 
+  const totalPrices = await Promise.all(orderItemsIdsResolved.map(async(orderItemId) => {
+    const orderItem = await OrderItem.findById(orderItemId).populate('product', 'price');
+    const totalPrice = orderItem.product.price * orderItem.quantity;
+    return totalPrice;
+  }))
+
+  const totalPrice = totalPrices.reduce((a,b) => a + b, 0)
+
+  console.log(totalPrices);
+
   let order = new Order({
     orderItems: orderItemsIdsResolved,
     shippingAddress1: req.body.shippingAddress1,
@@ -51,7 +61,7 @@ router.post('/', async (req, res) => {
     country: req.body.country,
     phone: req.body.phone,
     status: req.body.status,
-    totalPrice: req.body.totalPrice,
+    totalPrice: totalPrice,
     user: req.body.user
   });
   order = await order.save();
